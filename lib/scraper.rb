6415a6970d4164bb9cbce87b1b44f4a982e656c3
Pancho Labrador <plabradorjr@gmail.com>
2019-11-09 23:49:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,8 +18,7 @@ class Scraper
   end
 
   def get_page
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-
-    scraping/courses"))
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
 
     binding.pry
   end
